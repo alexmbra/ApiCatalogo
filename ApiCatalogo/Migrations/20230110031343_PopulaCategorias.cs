87ffd7114127f32276605dd9c66382e35e8d25aa
Alex Sander Novaes de Mesquita <alexmbra@hotmail.com>
2023-01-10 03:26:39 +0000

    Populate tables

---
+++ ApiCatalogo/Migrations/20230110031343_PopulaCategorias.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mB)
+        {
+            mB.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Bebidas', 'bebidas.jpg')");
+            mB.Sql("Insert into Categorias(Nome, ImagemUrl) Values('lanches', 'lanches.jpg')");
+            mB.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Sobremesas', 'Sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mB)
+        {
+            mB.Sql("delete from Categorias");
+        }
+    }
+}
---
+++ ApiCatalogo/Migrations/20230110031750_PopulateProdutos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    public partial class PopulateProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder mB)
+        {
+            mB.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values('Coca-cola Diet','Refrigerante de Cola 350 ml', 5.45, 'cocacola.jpg', 50, now(), 1)");
+
+            mB.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values('Lanche de atum','Lanche de atum com maionese', 8.50, 'atum.jpg', 10, now(), 2)");
+
+            mB.Sql("Insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                "Values('Pudim 100g','Pudim de leite condensado 100g', 6.75, 'pudim.jpg', 20, now(), 3)");
+        }
+
+        protected override void Down(MigrationBuilder mB)
+        {
+            mB.Sql("delete from Produtos");
+        }
+    }
+}
